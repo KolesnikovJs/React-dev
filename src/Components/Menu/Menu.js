fa696465093cdcd1cdec5d25618b55084c28c9a4
Vitalik Kolesnikov <webkolesnikov77@gmail.com>
2021-02-11 12:10:30 +0300

    end

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import dbMenu from '../DBMenu';
 import { ListItem } from './ListItem';
 import { Banner } from './Banner';
+import { useFetch } from '../Hooks/useFetch';
 
 const MenuStyled = styled.main`
     background-styled: #ccc;
@@ -14,23 +14,36 @@ const SectionMenu = styled.section`
     padding: 30px;
 `;
 
-export const Menu = ( {setOpenItem} ) => (
-    <MenuStyled>
-        <Banner/>
-        <SectionMenu>
-            <h2>Бургеры</h2>
-            <ListItem 
-            itemList={dbMenu.burger}
-            setOpenItem={setOpenItem}/>
+export const Menu = ( {setOpenItem} ) => {
 
-        </SectionMenu>
+    const res = useFetch();
+    const dbMenu = res.responce;
 
-        <SectionMenu>
-            <h2>Закуски и Напитки</h2>
-            <ListItem 
-            itemList={dbMenu.other}
-            setOpenItem={setOpenItem}/>/>
-        </SectionMenu>
-    </MenuStyled>
+    return  (
+        <MenuStyled>
+            <Banner/>
+            {res.responce ?
+            <>
+            <SectionMenu>
+                <h2>Бургеры</h2>
+                <ListItem 
+                itemList={dbMenu.burger}
+                setOpenItem={setOpenItem}/>
+    
+            </SectionMenu>
+    
+            <SectionMenu>
+                <h2>Закуски и Напитки</h2>
+                <ListItem 
+                itemList={dbMenu.other}
+                setOpenItem={setOpenItem}/>/>
+            </SectionMenu>
+            </> : res.error ?
+            <div>Sorry , we will fix it soon</div> : 
+            <div>Loading...</div>
+            }
+        </MenuStyled>
+    
+    );
 
-);
+};
